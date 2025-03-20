39725565b435acd7c6c4cde365c187a90d1170d0
Lisandro Marquez <lichimarquez04@gmail.com>
2025-03-20 15:04:42 -0300

    ej0 del p2 listo

--- Practicos/Practico 1/ej0/fixstring.c
+++ Practicos/Practico 1/ej0/fixstring.c
@@ -4,23 +4,39 @@
 #include "fixstring.h"
 
 unsigned int fstring_length(fixstring s) {
-    /*
-     * COMPLETAR
-     *
-     */
+    unsigned int count = 0;
+    int i = 0;
+
+    while (s[i] != '\0') {
+        count++;
+        i++;
+    }
+    
+    return count;
 }
 
 bool fstring_eq(fixstring s1, fixstring s2) {
-    /*
-     * COMPLETAR
-     *
-     */
+    if (fstring_length(s1) != fstring_length(s2)) return false;
+
+    for (int i = 0; i < fstring_length(s1); i++) {
+        if (s1[i] != s1[2]) return false;
+    }
+
+    return true;
 }
 
 bool fstring_less_eq(fixstring s1, fixstring s2) {
-    /*
-     * COMPLETAR
-     *
-     */
+    if (fstring_eq(s1, s2)) return true;
+
+    unsigned int minor_length = (fstring_length(s1) < fstring_length(s2) ? fstring_length(s1) : fstring_length(s2));
+
+    for (int i = 0; i < minor_length; i++) {
+        if (s1[i] > s2[i]) return false;
+        if (s1[i] < s2[i]) return true;
+    }
+
+    if (fstring_length(s1) > fstring_length(s2)) return false;
+
+    return true;
 }
 
