5a7427bb6f43cfb5ffeb5d0646dcad85e6256b35
Lisandro Marquez <lichimarquez04@gmail.com>
2025-03-20 15:17:48 -0300

    ej0 del p1 terminado

--- Practicos/Practico 1/ej0/fixstring.c
+++ Practicos/Practico 1/ej0/fixstring.c
@@ -18,7 +18,7 @@ unsigned int fstring_length(fixstring s) {
 bool fstring_eq(fixstring s1, fixstring s2) {
     if (fstring_length(s1) != fstring_length(s2)) return false;
 
-    for (int i = 0; i < fstring_length(s1); i++) {
+    for (unsigned int i = 0; i < fstring_length(s1); i++) {
         if (s1[i] != s1[2]) return false;
     }
 
@@ -28,7 +28,7 @@ bool fstring_eq(fixstring s1, fixstring s2) {
 bool fstring_less_eq(fixstring s1, fixstring s2) {
     if (fstring_eq(s1, s2)) return true;
 
-    unsigned int minor_length = (fstring_length(s1) < fstring_length(s2) ? fstring_length(s1) : fstring_length(s2));
+    int minor_length = (fstring_length(s1) < fstring_length(s2) ? fstring_length(s1) : fstring_length(s2));
 
     for (int i = 0; i < minor_length; i++) {
         if (s1[i] > s2[i]) return false;
