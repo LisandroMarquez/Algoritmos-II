8723e697799c3c267be166c2733242b2f51d05ef
Lisandro Marquez <lichimarquez04@gmail.com>
2025-03-20 17:06:02 -0300

    funciona (por fin)

--- Practicos/Practico 1/ej0/fixstring.c
+++ Practicos/Practico 1/ej0/fixstring.c
@@ -1,15 +1,14 @@
 #include <stdbool.h>
 #include <assert.h>
+#include <stdio.h>
 
 #include "fixstring.h"
 
 unsigned int fstring_length(fixstring s) {
     unsigned int count = 0;
-    int i = 0;
 
-    while (s[i] != '\0') {
+    while (s[count] != '\0') {
         count++;
-        i++;
     }
     
     return count;
@@ -21,22 +20,29 @@ bool fstring_eq(fixstring s1, fixstring s2) {
     if (fstring_length(s1) != fstring_length(s2)) flag = false;
 
     for (unsigned int i = 0; i < fstring_length(s1); i++) {
-        if (s1[i] != s1[2]) flag = false;
+        if (s1[i] != s2[i]) flag = false;
     }
 
     return flag;
 }
 
 bool fstring_less_eq(fixstring s1, fixstring s2) {
-    bool flag = true;
+    bool flag = true, aux_flag = false;
 
-    int i = 0, minor_length = (fstring_length(s1) < fstring_length(s2) ? fstring_length(s1) : fstring_length(s2));
+    unsigned int i = 0, minor_length, s1_length = fstring_length(s1), s2_length = fstring_length(s2);
 
-    while (i < minor_length || !flag) {
-        if (s1[i] > s2[i]) flag = false;
+    if (s1_length <= s2_length) {
+        minor_length = s1_length;
+    } else {
+        minor_length = s2_length;
+        flag = false;
     }
+    while (i < minor_length && flag && !aux_flag) {
+        if (s1[i] > s2[i]) flag = false;
+        if (s1[i] < s2[i]) aux_flag = true;
 
-    if (fstring_length(s1) > fstring_length(s2)) flag = false;
+        i++;
+    }
 
     return flag;
 }
