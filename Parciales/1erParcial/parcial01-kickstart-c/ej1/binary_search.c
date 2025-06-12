ce18d4a9dcb7078d7d0aae07980c2e074a536b1f
Lisandro Marquez <lichimarquez04@gmail.com>
2025-06-12 19:59:12 -0300

    1er Parcial Tema C resuelto

--- Parciales/1erParcial/parcial01-kickstart-c/ej1/binary_search.c
+++ Parciales/1erParcial/parcial01-kickstart-c/ej1/binary_search.c
@@ -1,5 +1,28 @@
 #include "binary_search.h"
 
+int binary_search_rec(int a[], int x, int lft, int rgt) {
+    int mid, i; 
+ 
+    if (lft > rgt) i = -1; 
+    else if (lft <= rgt) {
+        mid = (lft + rgt) / 2;
+        if (x < a[mid]) {
+            // buscar hacia la izquierda 
+            i = binary_search_rec(a, x, lft, mid-1);
+        }
+        else if (x == a[mid]) {
+            // lo encontramos! 
+            i = mid;
+        }
+        else if (x > a[mid]) {
+            // buscar hacia la derecha 
+            i = binary_search_rec(a, x, mid+1, rgt);
+        } 
+    }
+
+    return i;
+}
+
 /**
  * @brief Posición de un valor en un arreglo ordenado.
  * 
@@ -14,7 +37,7 @@
  * @param x Valor a buscar.
  */
 int binary_search(int a[], int length, int x) {
-    // COMPLETAR
+    int i = binary_search_rec(a, x, 0, length - 1);
 
-    return 0;
+    return i;
 }
--- Parciales/1erParcial/parcial01-kickstart-c/ej2/queries.c
+++ Parciales/1erParcial/parcial01-kickstart-c/ej2/queries.c
@@ -12,7 +12,13 @@
  * @param year Año, entre FST_YEAR (1980) y LST_YEAR (2016).
  */
 int year_min_moisture(WeatherTable a, int year) {
-    // COMPLETAR
-
-    return 0;
+    int temp = -276447231;
+    for (int j = 0; j < MONTHS; j++) {
+        for (int k = 0; k < DAYS; k++) {
+            if (a[year-FST_YEAR][j][k]._moisture < temp) {
+                temp = a[year-FST_YEAR][j][k]._moisture;
+            }
+        }
+    }
+    return temp;
 }
